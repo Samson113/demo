88d789bfb64f7beb50a805d59c6f739ff96c1cee
Omar-projects-in-github <200107057@stu.sdu.edu.kz>
2023-04-07 12:13:34 +0600

    commit for deployment

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.demo;
 
+import com.example.demo.contorller.MainController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private MainController mainController;
 
 	@Test
-	void contextLoads() {
+	public void startTest() throws Exception{
+		assertThat(mainController).isNotNull();
+	}
+
+	/*@Test
+	void mainTest() throws Exception {
+		this.mockMvc.perform(post("/"))
+				.andDo(print())
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("http://localhost/home"));
+	}*/
+	@Test
+	public void loginTest() throws Exception {
+		this.mockMvc.perform(formLogin().user("12345678").password("12345678"))
+				.andDo(print())
+				.andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/"));
 	}
 
 }
